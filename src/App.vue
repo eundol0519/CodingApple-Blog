4781769a7568469c6cfe45fbd2a65edf1b17680c
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-26 11:46:41 +0900

    📝 : List Component Implements

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Navbar />
+  <List />
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import List from "./pages/List.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    List,
   },
 };
 </script>
---
+++ src/pages/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list">
+    <div :key="i" v-for="(item, i) in list">
+      <h5>{{ item.title }}</h5>
+      <p class="date">{{ item.date }}</p>
+      <p>{{ item.content }}</p>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+import { writingList } from "@/constants/writingList";
+
+export default {
+  name: "List",
+  data() {
+    return {
+      list: writingList,
+    };
+  },
+};
+</script>
+
+<style>
+.list {
+  margin: 50px 0;
+}
+
+.list h5 {
+  font-weight: 700;
+}
+
+.list .date {
+  color: darkgray;
+}
+
+.list hr {
+  border-color: #e1e1e1;
+}
+</style>
