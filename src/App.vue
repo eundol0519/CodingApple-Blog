3937db21013f426225c509dda7869fa79ddb4b16
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-26 11:30:32 +0900

    📰 : Navbar Implements

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <button type="button" class="btn btn-primary">Primary</button>
+  <Navbar />
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
+
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
@@ -16,6 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
